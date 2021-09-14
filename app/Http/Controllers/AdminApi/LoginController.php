fdf88d6770e0e5c843dddcc561f5ec15b5ea29e9
DESKTOP-S26BPHO\admin <fanhuilinl@163.com>
2021-09-14 17:40:40 +0800

    第一次提交

---
+++ app/Http/Controllers/AdminApi/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\AdminApi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        echo 1;
+    }
+}
