04bda6493d28c66c762719f665cb1f69dfbe63b0
DESKTOP-S26BPHO\admin <fanhuilinl@163.com>
2021-09-17 16:16:18 +0800

    修改数据库迁移和填充文件

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,7 +47,6 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            VerifyJwtToken::class
         ],
     ];
 
@@ -68,5 +67,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        'auth.jwt' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
     ];
 }
